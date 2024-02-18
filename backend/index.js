d23aef3b43b5712abe8b7af6c27d03fc1c5632df
MD AJHAR <md.ajhar07860@gmail.com>
2024-02-19 02:07:21 +0530

    storing data into the Database using mongoose  45
    storing data into the Database using mongoose , working with thunder client

--- backend/index.js
+++ backend/index.js
@@ -1,18 +1,17 @@
 const connectToMongo = require('./db');
+const express = require('express')
+
 
 connectToMongo();
-
-const express = require('express')
 const app = express()
 const port = 3000
+
+app.use(express.json())
 
 //Available Routes
 app.use('/api/auth', require('./routes/auth') )
 app.use('/api/notes', require('./routes/notes') )
 
-// app.get('/', (req, res) => {
-//   res.send('Hello azhar!')
-// })
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
