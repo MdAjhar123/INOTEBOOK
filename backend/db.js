a0b1222ff7053ec10ecdcd39ab1e5c8df82644e9
MD AJHAR <md.ajhar07860@gmail.com>
2024-02-18 22:37:31 +0530

    Add router and models 44
    Adding router and models folder in backend folder for using schemas like email, pass etc

--- backend/db.js
+++ backend/db.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 const mongoURI = "mongodb://localhost:27017"
 
-const connectToMongo=()=>{
+const connectToMongo= ()=>{
     mongoose.connect(mongoURI);
     console.log("connected to mongo successfully");
 }
--- backend/index.js
+++ backend/index.js
@@ -6,11 +6,14 @@ const express = require('express')
 const app = express()
 const port = 3000
 
-app.get('/', (req, res) => {
-  res.send('Hello azhar!')
-})
+//Available Routes
+app.use('/api/auth', require('./routes/auth') )
+app.use('/api/notes', require('./routes/notes') )
+
+// app.get('/', (req, res) => {
+//   res.send('Hello azhar!')
+// })
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
 })
-jkjjhj
