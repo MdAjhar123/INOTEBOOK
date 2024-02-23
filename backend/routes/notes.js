9f4073930b2c2cf03f3dc1225d33cf30fd63219a
MD AJHAR <md.ajhar07860@gmail.com>
2024-02-24 00:16:43 +0530

    Updating an existing Note 53
    Updating an existing Note.

--- backend/routes/notes.js
+++ backend/routes/notes.js
@@ -5,7 +5,7 @@ const Note = require('../models/Note');
 const { body, validationResult } = require('express-validator');
 
 
-// ROUTE 1: Get All the notes using: GET "/api/auth/fetchallnotes". Login required
+// ROUTE 1: Get All the notes using: GET "/api/notes/fetchallnotes". Login required
 router.get('/fetchallnotes', fetchuser, async (req, res) => {
     try {
         const notes = await Note.find({ user: req.user.id })
@@ -17,7 +17,7 @@ router.get('/fetchallnotes', fetchuser, async (req, res) => {
 
 })
 
-// ROUTE 2: Add a new Notes using: POST "/api/auth/addnote". Login required
+// ROUTE 2: Add a new Notes using: POST "/api/notes/addnote". Login required
 router.post('/addnote', fetchuser, [
     body('title', 'Enter a valid title').isLength({ min: 3 }),
     body('description', 'Description must be atleast 5 characters').isLength({ min: 5 }),], async (req, res) => {
@@ -41,5 +41,28 @@ router.post('/addnote', fetchuser, [
             res.status(500).send("Internal Server Error");
         }
     })
+
+    // ROUTE 3: Update an existing Note using: POST "/api/notes/updatenote". Login required
+    router.put('/updatenote/:id', fetchuser, async (req, res) => {
+        const{title, description, tag} = req.body;
+        //Create a newNote object
+        const newNote = {};
+        if(title){newNote.title = title};
+        if(description){newNote.description = description};
+        if(tag){newNote.tag = tag};
+
+        //find the note to be updated and update it
+        let note = await Note.findById(req.params.id);
+        if(!note){return res.status(404).send("Not Found")}
+
+        if(note.user.toString() !== req.user.id){
+            return res.status(401).send("Not Allowed")
+        }
+
+        note = await Note.findByIdAndUpdate(req.params.id, {$set: newNote}, {new: true});
+        res.json({note});
+
+    })        
+
 
 module.exports = router
