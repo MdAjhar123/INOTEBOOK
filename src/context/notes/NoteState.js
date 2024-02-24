420e71e677a9a1913bf609cdc1823ded62c88487
MD AJHAR <md.ajhar07860@gmail.com>
2024-02-25 00:23:55 +0530

    Adding Note item in a separate Note component
    Adding Note item in a separate Note component 61

--- src/context/notes/NoteState.js
+++ src/context/notes/NoteState.js
@@ -21,7 +21,61 @@ const NoteState = (props)=>{
           "tag": "personal",
           "date": "2024-02-23T11:07:52.417Z",
           "__v": 0
-        }
+        },
+        {
+          "_id": "65d87c84c5d4c43bdc53c0c0",
+          "user": "65d76706e95fc2d7e4619089",
+          "title": "My title",
+          "description": "Please wake up early",
+          "tag": "personal",
+          "date": "2024-02-23T11:07:48.694Z",
+          "__v": 0
+        },
+        {
+          "_id": "65d87c88c5d4c43bdc53c0c2",
+          "user": "65d76706e95fc2d7e4619089",
+          "title": "My title",
+          "description": "Please wake up early",
+          "tag": "personal",
+          "date": "2024-02-23T11:07:52.417Z",
+          "__v": 0
+        },
+        {
+          "_id": "65d87c84c5d4c43bdc53c0c0",
+          "user": "65d76706e95fc2d7e4619089",
+          "title": "My title",
+          "description": "Please wake up early",
+          "tag": "personal",
+          "date": "2024-02-23T11:07:48.694Z",
+          "__v": 0
+        },
+        {
+          "_id": "65d87c88c5d4c43bdc53c0c2",
+          "user": "65d76706e95fc2d7e4619089",
+          "title": "My title",
+          "description": "Please wake up early",
+          "tag": "personal",
+          "date": "2024-02-23T11:07:52.417Z",
+          "__v": 0
+        },
+        {
+          "_id": "65d87c84c5d4c43bdc53c0c0",
+          "user": "65d76706e95fc2d7e4619089",
+          "title": "My title",
+          "description": "Please wake up early",
+          "tag": "personal",
+          "date": "2024-02-23T11:07:48.694Z",
+          "__v": 0
+        },
+        {
+          "_id": "65d87c88c5d4c43bdc53c0c2",
+          "user": "65d76706e95fc2d7e4619089",
+          "title": "My title",
+          "description": "Please wake up early",
+          "tag": "personal",
+          "date": "2024-02-23T11:07:52.417Z",
+          "__v": 0
+        },
     ]
     const [notes, setNotes] = useState(notesInitial)
 
