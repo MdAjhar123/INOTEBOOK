ee91f74c2c40b582a71b759c8ba47f8e6bdce0ca
MD AJHAR <md.ajhar07860@gmail.com>
2024-03-03 18:00:06 +0530

    Adding Signup component
    Adding Signup component  70

--- src/components/Login.js
+++ src/components/Login.js
@@ -8,7 +8,7 @@ const Login = () => {
 
     const handleSubmit = async (e) =>{
         e.preventDefault();
-        const response = await fetch(`http://localhost:5000/api/auth/login`, {
+        const response = await fetch("http://localhost:5000/api/auth/login", {
             method: "POST",
             headers: {
               "Content-Type": "application/json"
@@ -26,11 +26,12 @@ const Login = () => {
             alert("Invalid credentials")
         }
     }
-
+    
     const onChange = (e)=>{
         setCredentials({...credentials, [e.target.name]: e.target.value})
     }
 
+    
     return (
         <div>
             <form onSubmit={handleSubmit}>
